edd1fae672fb825619a54f1ede0b43cc8c1f6d6c
Crogen0407 <mymcoding0407@gmail.com>
2025-01-29 23:05:07 +0900

    feat : Height map

---
+++ version.002/Client/SamplerDemo.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "IExecute.h"
+#include "Geometry.h"
+class SamplerDemo : public IExecute
+{
+public:
+	void Init() override;
+	void Update() override;
+	void Render() override;
+
+private:
+	// Object
+	shared_ptr<Shader> _shader;
+	shared_ptr<Geometry<VertexTextureData>> _geometry;
+	shared_ptr<VertexBuffer> _vertexbuffer;
+	shared_ptr<IndexBuffer> _indexBuffer;
+
+	Matrix _world = Matrix::Identity;
+	Matrix _view = Matrix::Identity;
+	Matrix _projection = Matrix::Identity;
+
+	// Camera
+	shared_ptr<GameObject> _camera;
+
+	shared_ptr<Texture> _texture;
+};
